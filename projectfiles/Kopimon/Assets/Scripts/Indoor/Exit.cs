2962e38ca480bf630f6ccb108deb4813acfdeb16
Swapnil <swapnil.bapat10@gmail.com>
2022-04-18 01:22:53 +0800

    Removed all project files for reupload

--- projectfiles/Kopimon/Assets/Scripts/Indoor/Exit.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;  
-using System.Collections.Generic;  
-using UnityEngine;  
-public class Exit: MonoBehaviour {  
-    public void exitgame() {  
-        Debug.Log("exitgame");  
-        Application.Quit();  
-    }  
-}   
